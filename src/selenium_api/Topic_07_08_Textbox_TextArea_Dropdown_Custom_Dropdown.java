39f122889aebd4968b88fbc87ae9cbc21b2877db
AD <AD@192.168.1.17>
2019-09-15 14:16:47 +0700

    Topic 07 TC 01 02 submit

--- src/selenium_api/Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown.java
+++ src/selenium_api/Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -17,20 +18,19 @@ import org.testng.annotations.AfterClass;
 public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 	WebDriver driver;
 
-
 	String userVal = "mngr209708";
 	String passVal = "arasYsY";
-	String cusName,dateOfbirth,address,city,state,pin,mobiNumber,email,password;
-	String editAddress,editCity,editState,editPin,editMobiNumber,editEmail;
-	
+	String cusName, dateOfbirth, address, city, state, pin, mobiNumber, email, password;
+	String editAddress, editCity, editState, editPin, editMobiNumber, editEmail;
+
 	By userId = By.xpath("//input[@name='uid']");
 	By passId = By.xpath("//input[@name='password']");
 	By btnLogin = By.xpath("//input[@name='btnLogin']");
 	By homePageVal = By.xpath("//marquee[@class='heading3']");
 	By newCusPage = By.xpath("//a[@href='addcustomerpage.php']");
 	By cusNameTxt = By.xpath("//input[@name='name']");
-	By genderRad = By.xpath("//input[@value='m']");
-	By dopTxt = By.xpath("//input[@id='dob']");
+	By genderRad = By.xpath("//input[@name='gender']");
+	By dopTxt = By.xpath("//input[@name='dob']");
 	By addrTat = By.xpath("//textarea[@onblur='validateAddress();']");
 	By cityTxt = By.xpath("//input[@onblur='validateCity();']");
 	By stateTxt = By.xpath("//input[@onblur='validateState();']");
@@ -42,7 +42,7 @@ public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 	By cusNameId = By.xpath("//td[text()='Customer ID']");
 	By cusNameIdVal = By.xpath("//td[text()='Customer ID']/following-sibling::td");
 	By cusNameVal = By.xpath("//td[text()='Customer Name']/following-sibling::td");
-	By genderVal = By.xpath("///td[text()='Gender']/following-sibling::td");
+	By genderVal = By.xpath("//td[text()='Gender']/following-sibling::td");
 	By dobVal = By.xpath("//td[text()='Birthdate']/following-sibling::td");
 	By addVal = By.xpath("//td[text()='Address']/following-sibling::td");
 	By cityVal = By.xpath("//td[text()='City']/following-sibling::td");
@@ -53,16 +53,15 @@ public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 	By editCusPage = By.xpath("//a[@href='EditCustomer.php']");
 	By editCusIdTxt = By.xpath("//input[@name='cusid']");
 	By editSubmitBtn = By.xpath("//input[@name='AccSubmit']");
-	
+
 	By job01DDL = By.xpath("//select[@id='job1']");
-	
-	By jqueryPage = By.xpath("//span[@id='number-button']/span[@class='ui-selectmenu-text']");
+
+	By jqueryPage01 = By.xpath("//span[@id='number-button']/span[contains(@class,'ui-selectmenu-icon')]");
 	By agularPage = By.xpath("//mat-select[@id='mat-select-5']//span[contains(@class,'ng-tns-c21-18')]");
 	By reactPage = By.xpath("//div[text()='Select Friend']");
 	By vuePage = By.xpath("//li[@class='dropdown-toggle']");
 	By editPage = By.xpath("//div[@id='default-place']");
 
-	
 	@BeforeClass
 	public void beforeClass() {
 
@@ -72,17 +71,18 @@ public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 
 		System.out.println("Pre-condition - Step 02: Wait for page loading success");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		cusName = "Kerrin Laird-Craig";
-		dateOfbirth = "10/19/2018";
+		driver.manage().window().maximize();
+
+		cusName = "Kerrin Laird Craig";
+		dateOfbirth = "2018-10-19";
 		address = "50 Cambridge Plaza";
 		city = "Columbus";
 		state = "Ohio";
 		pin = "441066";
 		mobiNumber = "6141912444";
-		email = "klairdcraig0@deliciousdays.com";
+		email = "amacsporr@eativecommons.org";
 		password = "nId5rmqZrR";
-		
+
 		editAddress = "1 Mccormick Hill";
 		editCity = "Beaufort";
 		editState = "South Carolina";
@@ -92,29 +92,29 @@ public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 	}
 
 	@Test
+
 	public void TC01_Handle_Textbox_Textarea() throws Exception {
-		String expHomePageVal = "Welcome To Manager's Page of Guru99 Bank"; 
-		String cusID ;
+		String expHomePageVal = "Welcome To Manager's Page of Guru99 Bank";
+		String cusID;
 		String gender = "male";
 		String pageUrl = "http://demo.guru99.com/v4/";
-		
+
 		System.out.println("Pre-condition : Open Page url");
 		driver.get(pageUrl);
-		
-		//Đăng nhập tài khoản
+
+		// Đăng nhập tài khoản
 		sendkeyElement(userId, userVal);
 		sendkeyElement(passId, passVal);
 		clickToElement(btnLogin);
-		
-		//Check đăng nhập thành công
+
+		// Check đăng nhập thành công
 		Thread.sleep(3000);
 		Assert.assertEquals(getTextElement(homePageVal), expHomePageVal);
-		
-		//Chọn menu New Cusomer
-		clickToElement(newCusPage);
-		Thread.sleep(2000);
-		
-		//Nhập thông tin Customer và submit
+		System.out.println("Step 01 : Home Page Value :" + getTextElement(homePageVal));
+
+		// Chọn menu New Cusomer clickToElement(newCusPage); Thread.sleep(2000);
+
+		// Nhập thông tin Customer và submit
 		sendkeyElement(cusNameTxt, cusName);
 		sendkeyElement(dopTxt, dateOfbirth);
 		sendkeyElement(addrTat, address);
@@ -126,10 +126,11 @@ public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 		sendkeyElement(passTxt, password);
 		clickToElement(newSubmitBtn);
 		Thread.sleep(2000);
-		
-		//Lấy thông tin Cusomer và so sánh với input
-		cusID = getTextElement(cusNameId);
-		Assert.assertEquals(getTextElement(cusNameIdVal), cusName);
+		System.out.println("Step 02 : Add value to New Customer form Successfully !!");
+
+		// Lấy thông tin Cusomer và so sánh với input
+		cusID = getTextElement(cusNameIdVal);
+		Assert.assertEquals(getTextElement(cusNameVal), cusName);
 		Assert.assertEquals(getTextElement(genderVal), gender);
 		Assert.assertEquals(getTextElement(dobVal), dateOfbirth);
 		Assert.assertEquals(getTextElement(addVal), address);
@@ -138,26 +139,29 @@ public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 		Assert.assertEquals(getTextElement(pinval), pin);
 		Assert.assertEquals(getTextElement(mobiNumVal), mobiNumber);
 		Assert.assertEquals(getTextElement(emailVal), email);
-		
-		//Chọn menu Edit Customer
+		System.out.println("Step 03 : Compare New Customer Input Anh Output Value Successfully");
+
+		// Chọn menu Edit Customer
 		clickToElement(editCusPage);
 		Thread.sleep(2000);
 		sendkeyElement(editCusIdTxt, cusID);
 		clickToElement(editSubmitBtn);
 		Thread.sleep(2000);
+		System.out.println("Step 04 : Navigate to Edit Customer form Successfully !!");
 
-		//So sánh thông tin edit hiển thị với input
-		Assert.assertEquals(getTextElement(cusNameTxt), cusName);
-		Assert.assertEquals(getTextElement(genderRad), gender);
-		Assert.assertEquals(getTextElement(dopTxt), dateOfbirth);
-		Assert.assertEquals(getTextElement(addrTat), address);
-		Assert.assertEquals(getTextElement(cityTxt), city);
-		Assert.assertEquals(getTextElement(stateTxt), state);
-		Assert.assertEquals(getTextElement(pinTxt), pin);
-		Assert.assertEquals(getTextElement(mobiNumTxt), mobiNumber);
-		Assert.assertEquals(getTextElement(emailTxt), email);
-		
-		//Nhập thông tin Customer mới
+		// So sánh thông tin edit hiển thị với input
+		Assert.assertEquals(getAttributeElement(cusNameTxt), cusName);
+		Assert.assertEquals(getAttributeElement(genderRad), gender);
+		Assert.assertEquals(getAttributeElement(dopTxt), dateOfbirth);
+		Assert.assertEquals(getAttributeElement(addrTat), address);
+		Assert.assertEquals(getAttributeElement(cityTxt), city);
+		Assert.assertEquals(getAttributeElement(stateTxt), state);
+		Assert.assertEquals(getAttributeElement(pinTxt), pin);
+		Assert.assertEquals(getAttributeElement(mobiNumTxt), mobiNumber);
+		Assert.assertEquals(getAttributeElement(emailTxt), email);
+		System.out.println("Step 05 : Compare Customer Input Anh Edit Output Value Successfully");
+
+		// Nhập thông tin Customer mới
 		sendkeyElement(addrTat, editAddress);
 		sendkeyElement(cityTxt, editCity);
 		sendkeyElement(stateTxt, editState);
@@ -166,10 +170,11 @@ public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 		sendkeyElement(emailTxt, editEmail);
 		clickToElement(newSubmitBtn);
 		Thread.sleep(2000);
-		
-		//So sánh thông tin mới với input
-		
-		Assert.assertEquals(getTextElement(cusNameIdVal), cusName);
+		System.out.println("Step 06 : Edit Custoemr Successfully !!");
+
+		// So sánh thông tin mới với input
+
+		Assert.assertEquals(getTextElement(cusNameVal), cusName);
 		Assert.assertEquals(getTextElement(genderVal), gender);
 		Assert.assertEquals(getTextElement(dobVal), dateOfbirth);
 		Assert.assertEquals(getTextElement(addVal), editAddress);
@@ -178,102 +183,119 @@ public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 		Assert.assertEquals(getTextElement(pinval), editPin);
 		Assert.assertEquals(getTextElement(mobiNumVal), editMobiNumber);
 		Assert.assertEquals(getTextElement(emailVal), editEmail);
-		
+		System.out.println("Step 07 : Compare Customer Edit Input Anh Edit Output Value Successfully");
 	}
-	
+
 	@Test
+
 	public void TC02_Handle_HTML_Dropdownlist() throws Exception {
-		
+
 		String pageUrl = "https://daominhdam.github.io/basic-form/index.html";
 		String autoValue = "Automation Tester";
 		String manualValue = "manual";
-		
+		WebElement jo01;
+		String j01;
+
 		System.out.println("Pre-condition : Open Page url");
 		driver.get(pageUrl);
 		Thread.sleep(2000);
-		
+
 		// Kiểm tra Job 01 có hỗ trợ mutliselect không
-		Select job01 = new Select( driver.findElement(job01DDL));
-		if(job01.isMultiple() == true) {
-			System.out.println("Job Role 01 Dropdownlist is multi-select");
+		Select job01 = new Select(driver.findElement(job01DDL));
+		if (job01.isMultiple() == true) {
+			System.out.println("Step 01 : Job Role 01 Dropdownlist is multi-select");
 		} else {
-			System.out.println("Job Role 01 Dropdownlist is NOT multi-select");
+			System.out.println("Step 01 : Job Role 01 Dropdownlist is NOT multi-select");
 		}
-		
+
 		// Chọn giá trị trong Dropdownlist bằng selectByVisibleText
 		job01.selectByVisibleText(autoValue);
-		System.out.println("Job Role 01 Dropdownlist value is : "+ driver.findElement(job01DDL).getText());
-		
+		jo01 = job01.getFirstSelectedOption();
+		j01 = jo01.getText();
+		System.out.println("Step 02 : Job Role 01 Dropdownlist value is : " + j01);
+
 		// Chọn giá trị trong Dropdownlist bằng selectByValue
 		job01.selectByValue(manualValue);
-		System.out.println("Job Role 01 Dropdownlist value is : "+ driver.findElement(job01DDL).getText());
-		
+		jo01 = job01.getFirstSelectedOption();
+		j01 = jo01.getText();
+		System.out.println("Step 03 : Job Role 01 Dropdownlist value is : " + j01);
+
 		// Chọn giá trị trong Dropdownlist bằng selectIndex
 		job01.selectByIndex(3);
-		System.out.println("Job Role 01 Dropdownlist value is : "+ driver.findElement(job01DDL).getText());
-		
-		//Đếm số lượng giá trị có trong Dropdownlist 
+		jo01 = job01.getFirstSelectedOption();
+		j01 = jo01.getText();
+		System.out.println("Step 04 : Job Role 01 Dropdownlist value is : " + j01);
+
+		// Đếm số lượng giá trị có trong Dropdownlist
 		List<WebElement> jo01All = job01.getOptions();
 		Assert.assertEquals(jo01All.size(), 5);
-		
+		System.out.println("Step 05 : Total Job Role 01 Dropdownlist : " + jo01All.size() + " values");
+
 	}
-	
+
 	@Test
 	public void TC03_Handle_Custom_Dropdownlist() throws Exception {
-		
+
 		String jqueryPageUrl = "https://jqueryui.com/resources/demos/selectmenu/default.html";
 		String agularpageUrl = "https://material.angular.io/components/select/examples";
 		String reactJSPageUrl = "https://react.semantic-ui.com/modules/dropdown/";
 		String vuePageUrl = "https://mikerodham.github.io/vue-dropdowns/";
-		String editPageUrl = "http://indrimuska.github.io/jquery-editable-select/";
-		
-		System.out.println(handleDropAndGetElement(jqueryPage, "19", jqueryPageUrl, "Jquery Page"));
-		
-		System.out.println(handleDropAndGetElement(agularPage, "Arkansas", agularpageUrl, "Agular Page"));
-		
-		System.out.println(handleDropAndGetElement(reactPage, "Stevie Feliciano", reactJSPageUrl, "Reacr JS Page"));
-		
-		System.out.println(handleDropAndGetElement(vuePage, "Second Option", vuePageUrl, "Vue JS Page"));
-		
-		System.out.println("Pre-condition : Open React Editable Page url");
-		driver.get(editPageUrl);
+		// String editPageUrl = "http://indrimuska.github.io/jquery-editable-select/";
+
+		// System.out.println(handleDropAndGetElement(jqueryPage, "19", jqueryPageUrl,
+		// "Jquery Page"));
+
+		/*
+		 * System.out.println(handleDropAndGetElement(agularPage, "Arkansas",
+		 * agularpageUrl, "Agular Page"));
+		 * 
+		 * System.out.println(handleDropAndGetElement(reactPage, "Stevie Feliciano",
+		 * reactJSPageUrl, "Reacr JS Page"));
+		 * 
+		 * System.out.println(handleDropAndGetElement(vuePage, "Second Option",
+		 * vuePageUrl, "Vue JS Page"));
+		 */
+		System.out.println("Pre-condition : Open Page url");
+		driver.get(jqueryPageUrl);
 		Thread.sleep(2000);
-		
-		// Tìm và chọn giá trị trong Dropdownlist reactJS
-		
-		
-		
-		
-		
-		
+		driver.findElement(jqueryPage01).click();
+
 	}
-	public void sendkeyElement(By by,String value) {
+
+	public void sendkeyElement(By by, String value) {
 		driver.findElement(by).clear();
 		driver.findElement(by).sendKeys(value);
 	}
-	
+
 	public void clickToElement(By by) {
 		driver.findElement(by).click();
 	}
-	
+
 	public String getTextElement(By by) {
 		return driver.findElement(by).getText();
 	}
-	
-	public String handleDropAndGetElement(By by,String dataValue,String pageUrl,String pageValue) throws Exception {
-		
-		System.out.println("Pre-condition : Open"+pageValue+" url");
-		driver.get(pageUrl);
-		Thread.sleep(2000);
-		
-		// Tìm và chọn giá trị trong Dropdownlist reactJS
-		Select vueElement = new Select(driver.findElement(by));
-		vueElement.selectByVisibleText(dataValue);
-		String result = "Select a "+pageValue+" Dropdownlist value is : "+ driver.findElement(by).getText();
-		
-		return result;
+
+	public String getAttributeElement(By by) {
+		return driver.findElement(by).getAttribute("value");
 	}
-	
+
+	/*
+	 * public String handleDropAndGetElement(By by, String dataValue, String
+	 * pageUrl, String pageValue) throws Exception {
+	 * 
+	 * WebElement element ; String option;
+	 * 
+	 * System.out.println("Pre-condition : Open" + pageValue + " url");
+	 * driver.get(pageUrl); Thread.sleep(2000);
+	 * 
+	 * // Tìm và chọn giá trị trong Dropdownlist reactJS Select pageElement = new
+	 * Select(driver.findElement(by)); pageElement.selectByVisibleText(dataValue);
+	 * element = pageElement.getFirstSelectedOption(); option = element.getText();
+	 * String result = "Select a " + pageValue + " Dropdownlist value is : " +
+	 * option;
+	 * 
+	 * return result; }
+	 */
 
 	@AfterClass
 	public void afterClass() {
@@ -283,5 +305,5 @@ public class Topic_07_08_Textbox_TextArea_Dropdown_Custom_Dropdown {
 		System.out.println();
 		driver.quit();
 	}
-	
+
 }
